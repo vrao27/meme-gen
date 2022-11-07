1f3f63e8841bdcde061b80325ba2e552c962922a
Vinay Rao <vrao8912@gmail.com>
2022-11-07 11:41:44 +0100

    axios call func

--- src/components/Memes.js
+++ src/components/Memes.js
@@ -1,13 +1,24 @@
-import React from 'react'
+import {useState, useEffect} from "react";
+import axios from 'axios';
 
-const myMemes = () => {
 
+const baseURL = "https://api.imgflip.com/get_memes";
+
+const [meme, setMeme] = useState();
+  
+const getMemes = () => { 
+    axios.get(baseURL)
+    .them((response) => {
+        console.log(response);
+        const myMeme = response.data;
+        setMeme(myMeme);
+    });
     
-
+    useEffect(() => getMemes(), []);
 
   return (
     <div>Memes</div>
   )
 }
 
-export default Memes
+export default Memes}
